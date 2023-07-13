3cfbc5d1a5404c9e84252bffd0f2fc3ab4f5a322
Gabriel Dautreppe <gabriel.dautreppe@gmail.com>
2023-07-13 18:46:18 +0200

    ✨ custom border cards

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -1,27 +1,43 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appBorderCard]'
 })
 export class BorderCardDirective {
+  private initialColor: string = '#f5f5f5';
+  private defaultColor: string = '#009688';
+  private defaultHeight: number = 180;
+  private defaultBorderStyle: string = 'dotted';
+  private initialBorderStyle: string = 'double';
+
 
   constructor(private el: ElementRef) {
-    this.setBorder('#f5f5f5');
+    this.setBorder('solid');
+    this.setBorderColor('#f5f5f5');
     this.setHeight(180);
   }
+
+  @Input('appBorderCard') borderColor: string; // alias
+  @Input() borderStyle: string = this.defaultBorderStyle;
+
   @HostListener('mouseenter') onMouseEnter() {
-    this.setBorder('#009688');
+    this.setBorderColor(this.borderColor || this.defaultColor);
+    this.setBorder(this.borderStyle || this.defaultBorderStyle);
   }
 
   @HostListener('mouseleave') onMouseLeave() {
-    this.setBorder('#f5f5f5');
+    this.setBorderColor(this.initialColor);
+    this.setBorder(this.initialBorderStyle);
   }
 
   setHeight(height: number) {
     this.el.nativeElement.style.height = `${height}px`;
   }
-  setBorder(color: string) {
+  setBorderColor(color: string) {
     this.el.nativeElement.style.border = `solid 4px ${color}`;
   }
 
+  setBorder(style: string) {
+    this.el.nativeElement.style.borderStyle = `${style}`;
+}
 }
