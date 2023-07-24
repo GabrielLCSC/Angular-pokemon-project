d6cee3c01918b87b34f5a47e57672019c515fbd6
Gabriel Dautreppe <gabriel.dautreppe@gmail.com>
2023-07-24 21:30:44 +0200

    :fix: correc routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
+import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
 
 const routes: Routes = [
-  { path: '/pokemons', component: ListPokemonComponent },
-  { path: '/pokemon/:id', component: ListPokemonComponent },
-  { path: '', redirectTo: '/pokemons', pathMatch: 'full' }
+  { path: 'pokemons', component: ListPokemonComponent },
+  { path: '/pokemon/:id', component: DetailPokemonComponent },
+  { path: '', redirectTo: 'pokemons', pathMatch: 'full' }
 ];
 
 @NgModule({
