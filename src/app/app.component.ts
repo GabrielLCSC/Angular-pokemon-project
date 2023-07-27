e7efdf8c2e53c99b7a9ed91e8cf2db7167dd84ce
Gabriel Dautreppe <gabriel.dautreppe@gmail.com>
2023-07-27 20:42:03 +0200

    :sparkles: clean code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { POKEMONS } from './mock-pokemon-list';
-import { Pokemon } from './pokemon';
+// import { POKEMONS } from './mock-pokemon-list';
+// import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styles: []
 })
 
-export class AppComponent implements OnInit {
-  pokemonList: Pokemon[] = POKEMONS;
-  pokemonSelected: Pokemon|undefined;
+export class AppComponent {
+  
+  // pokemonList: Pokemon[] = POKEMONS;
+  // pokemonSelected: Pokemon|undefined;
 
-  ngOnInit() {
-    console.table(this.pokemonList);
-  }
+  // ngOnInit() {
+  //   console.table(this.pokemonList);
+  // }
 
-  selectPokemon(pokemonId: string) {
-    const pokemon: Pokemon|undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId);
+  // selectPokemon(pokemonId: string) {
+  //   const pokemon: Pokemon|undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId);
 
-    if (pokemon) {
-      console.log(`Vous avez choisi ${pokemon.name}`);
-      this.pokemonSelected = pokemon;
-    } else {
-      console.log(`Le pokemon n'existe pas`); 
-      this.pokemonSelected = pokemon;
-    }
-  }
+  //   if (pokemon) {
+  //     console.log(`Vous avez choisi ${pokemon.name}`);
+  //     this.pokemonSelected = pokemon;
+  //   } else {
+  //     console.log(`Le pokemon n'existe pas`); 
+  //     this.pokemonSelected = pokemon;
+  //   }
+  // }
 }
